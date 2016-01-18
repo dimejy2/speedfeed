38994c1de510ef2d05d8ccfee26a6f365b7f5eb6
Dimeji Abidoye <dimejy2@gmail.com>
2016-01-18 01:42:17 -0500

    so close

--- routes/orders.js
+++ routes/orders.js
@@ -23,7 +23,7 @@ router.route('/')
             //console.log(req.body, order);
 
             order.save(function(err){
-                console.log("here");
+                console.log(err);
                 if(err){ return res.send(messages.genericError);
                 console.log(err);}
                 return res.send(messages.success);
