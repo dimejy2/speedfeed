68b9f92eb88d106d87c0400595230c20f3ea173c
Dimeji Abidoye <dimejy2@gmail.com>
2016-01-18 01:00:25 -0500

    fixed order model bug

--- models/order.js
+++ models/order.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose'),
         timestamps = require('mongoose-timestamps');
 
 var OrderSchema = new Schema({
-    phone_number :{type : String, required :true , index , true},
-    delivery_address ;{type : String, required, true}, 
+    phone_number :{type : String, required :true , index : true},
+    delivery_address ;{type : String, required: true}, 
     contents :{ type : String, required : true}
-    fulfilled :{type : Boolean, required : true, index , true}
+    fulfilled :{type : Boolean, required : true, index : true}
 });
 
 OrderSchema.plugin(timestamps);
